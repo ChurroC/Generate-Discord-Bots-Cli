2f17670a2324e3b7dca94dc33e53a257de3c440a
Charan <charanvenkatchandran@gmail.com>
2023-02-11 22:48:54 -0600

    Brother is getting glasses

--- slashCommands/interactionExamples/context.js
+++ slashCommands/interactionExamples/context.js
@@ -8,6 +8,7 @@ module.exports = {
     data: new ContextMenuCommandBuilder()
         .setName("Say Hi")
         .setType(ApplicationCommandType.Message),
+    // Message ineraction or user interaction depending]
     async execute(interaction) {
         console.log(interaction.targetUser.username);
         console.log(interaction.options.getUser("target"));
