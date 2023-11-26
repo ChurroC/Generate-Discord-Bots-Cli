3c6669bcecb8ab9a3eb2e8deffae07720ba95504
ChurroC <charanvenkatchandran@gmail.com>
2023-11-25 22:09:16 -0600

    EXPLAIN ASYNC AND AWAIT. Ping.js shows why await is important. Also added some todo.

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -3,13 +3,14 @@ module.exports = {
     name: ["ping", "p"],
     description: "Replies with Pong!",
     async execute(message, args, client, db) {
-        await message.reply(
+        const test = await message.reply(
             `Pong!\nLatency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(
                 client.ws.ping
             )}ms`
         );
-        await message.reply(bhj);
-        await db.member.update({
+        console.log(test);
+        test.reply("bhj");
+        db.member.update({
             where: {
                 memberId: message.member.id,
             },
